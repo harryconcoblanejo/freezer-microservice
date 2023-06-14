28922ca227c76dedcd53bcdf04f1f2a0f68d46ef
Rodrigo Lopez <harryconcoblanejou@gmail.com>
2023-06-14 16:16:46 -0300

    ya funciona

--- src/models/freezerModel.js
+++ src/models/freezerModel.js
@@ -5,17 +5,26 @@ const freezerModel = {
     return res.send("recibiendo data");
   },
   sendFreezerData: async (req, res) => {
+    try {
     const { data1 } = req.body;
     const token = req.headers.authorization; // usar este
     const tokenTest = 123;
     //verify token
-    const authorized = verifyToken(tokenTest);
+    const authorized = verifyToken(tokenTest); 
     if (authorized) {
-      //meter todo el codigo aca
-      return res.send({ data1, msj: "lalala" });
-    } else {
-      throw new Error("Unauthorized device!!");
+        //meter todo el codigo aca
+        return res.send({ data1, msj: "lalala" });
+      }
+      else{
+        res.send({msj:' device no autorizado'})
+        throw new Error(' device no autorizado')
+      }
+    } catch (error) {
+        console.log(error)
+       
     }
+   
+  
   },
 };
 

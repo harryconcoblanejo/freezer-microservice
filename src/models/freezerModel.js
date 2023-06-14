929c9e914c6639d9c3b5479fda7b01b19533e9cb
Rodrigo Lopez <harryconcoblanejou@gmail.com>
2023-06-14 15:35:34 -0300

    cambio freezerModels(pongo el codigo dentro del if)

--- src/models/freezerModel.js
+++ src/models/freezerModel.js
@@ -5,18 +5,17 @@ const freezerModel = {
     return res.send("recibiendo data");
   },
   sendFreezerData: async (req, res) => {
-    const token = req.headers.authorization // usar este
-    const tokenTest=123
+    const token = req.headers.authorization; // usar este
+    const tokenTest = 123;
     //verify token
     const authorized = veryfyToken(tokenTest);
-    if(authorized){
-        //meter todo el codigo aca
-    const { data } = req.body;
-    return res.send({ data, msj: "lalala" });
-    }else{
-       throw new Error ("Unauthorized device!!"); 
+    if (authorized) {
+      //meter todo el codigo aca
+      const { data } = req.body;
+      return res.send({ data, msj: "lalala" });
+    } else {
+      throw new Error("Unauthorized device!!");
     }
-   
   },
 };
 

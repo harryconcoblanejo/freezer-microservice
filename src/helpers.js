4543b13921efeac5aaf6ea7db39a78fe66cb111c
Rodrigo Lopez <harryconcoblanejou@gmail.com>
2023-06-14 15:50:35 -0300

    error de sintaxis en importacion de verifyToken

--- src/helpers.js
+++ src/helpers.js
@@ -1,18 +1,21 @@
 const jwt = require("jsonwebtoken");
-// const signature = process.env.JWTS;
-const signature = 'superclave123'
+    // const signature = process.env.JWTS;
+    const signature = 'superclave123'
 
 export const verifyToken = function (token) {
-
+    
+console.log('llega este token');
+console.log(token)
   if (!token) {
     return false;
   }
   // verificar que sea el mismo token de la base de datos
   const bdtoken = 123 //consultar el token de la BD
-  if(token === bdtoken) {
+  if(token == bdtoken) {
    return true 
   }else{
     return false
   }
  
 };
+
--- src/models/freezerModel.js
+++ src/models/freezerModel.js
@@ -1,18 +1,18 @@
-import { veryfyToken } from "../helpers";
+import  {verifyToken } from "../helpers.js";
 
 const freezerModel = {
   getFreezerData: async (req, res) => {
     return res.send("recibiendo data");
   },
   sendFreezerData: async (req, res) => {
+    const { data1 } = req.body;
     const token = req.headers.authorization; // usar este
     const tokenTest = 123;
     //verify token
-    const authorized = veryfyToken(tokenTest);
+    const authorized = verifyToken(tokenTest);
     if (authorized) {
       //meter todo el codigo aca
-      const { data } = req.body;
-      return res.send({ data, msj: "lalala" });
+      return res.send({ data1, msj: "lalala" });
     } else {
       throw new Error("Unauthorized device!!");
     }

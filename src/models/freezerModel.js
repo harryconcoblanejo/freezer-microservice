27ebdf171c01562ef2a01b2d7ca6e111ace95ce0
Rodrigo Lopez <harryconcoblanejou@gmail.com>
2023-06-28 14:53:18 -0300

    creacion del objeto data y consulta a db

--- src/models/freezerModel.js
+++ src/models/freezerModel.js
@@ -1,70 +1,69 @@
 import { verifyToken } from "../helpers.js";
-let data ={
-  readings: {}
-}
+let data = {
+  readings: {},
+};
 const freezerModel = {
   getFreezerData: async (req, res) => {
-    return res.send('Recibiendo data desde freezerModel');
+    return res.send("Recibiendo data desde freezerModel");
   },
   sendFreezerData: async (req, res, db) => {
     try {
       let context = {};
       context.db = db;
-      const { readings} = req.body;
+      const { readings } = req.body;
       const token = req.headers.authorization;
-      const qr = req.headers['qr'];
+      const qr = req.headers["qr"];
 
-      // Verificar el token
       const authorized = await verifyToken(token, qr, db);
-
       const currentDay = getCurrentDay(); // Obtener el día actual en formato 'YYYY-MM-DD'
+      
+      let data;
+      //si existe la collection 'freezers'
+      let freezer_data = await db.collection('freezers').findOne({qr:qr})
+      console.log(freezer_data)
+      if(!freezer_data){
+        data = {
+          readings:{}
+        }
+      }else{
+        data=freezer_data.readings
+      }
 
-     /*  let freezer = {
-        assigned_to: 'COM-1',
-        device_id: 'FRE-1',
-        assigned_refrigerator: 'REF-1',
-        battery_level: battery_level,
-        readings: {}
-      }; */
-
-     
-
+      let params = data 
       // Verificar si ya existe un array de lecturas para el día correspondiente
-      if (!data.readings[currentDay]) {
-        data.readings[currentDay] = []; // Si no existe, crear un nuevo array vacío
+      if (!params.readings[currentDay]) {
+        params.readings[currentDay] = []; // Si no existe, crear un nuevo array vacío
       }
 
       // Agregar la lectura actual al array correspondiente al día
-    
-        readings.forEach(reading => {
-          console.log('en el for')
-          console.log(reading)
-          data.readings[currentDay].push({
-            value: reading.value,
-            timestamp: reading.timestamp
-          })
+
+      readings.forEach((reading) => {
+        console.log(reading);
+        params.readings[currentDay].push({
+          value: reading.value,
+          timestamp: reading.timestamp,
         });
-      console.log('salgo del for');
-      console.log(console.log(JSON.stringify(data, null, 2)))
+      });
+
+      console.log(console.log(JSON.stringify(data, null, 2)));
       if (authorized) {
-        
-        return res.send({ data, msj: 'Data enviada con éxito!' });
+        return res.send({ params, msj: "Data enviada con éxito!" });
       } else {
-        res.send({ msj: 'Dispositivo no autorizado' });
-        throw new Error('Dispositivo no autorizado');
+        res.send({ msj: "Dispositivo no autorizado" });
+        throw new Error("Dispositivo no autorizado");
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  },
 };
 
 // Función para obtener el día actual en formato 'YYYY-MM-DD'
 function getCurrentDay() {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
-  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-  const day = String(currentDate.getDate()).padStart(2, '0');
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 }
 

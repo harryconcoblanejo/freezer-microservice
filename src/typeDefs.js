f4041eeacbbd0f5620b4635fcf6679c255c69f59
Rodrigo Lopez <harryconcoblanejou@gmail.com>
2023-07-07 11:04:02 -0300

    dockerizando

--- src/typeDefs.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "apollo-server-express";
-import companyDefs from "./defSchemas/companyDefs";
-const typeDefs = gql`
-  type Query {
-    companies(belong_id: String): [Company]
-  }
-  ${companyDefs}
-`;
-export default typeDefs;
